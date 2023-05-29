2667efda8af53f8a8b334f7bb677123753de132f
ВашÐEGo87 <gokov87@vk.com>
2023-05-29 23:14:50 +0300

    version_7.0.0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Johnny", "Depp", (byte) 59);
+        userService.saveUser("Amber", "Heard", (byte) 37);
+        userService.saveUser("Bill", "Gates", (byte) 67);
+        userService.saveUser("Elon", "Musk", (byte) 51);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        Util.closeSession();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,57 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/dbhiber";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "R00T_Tdutybq_>hmtdbx";
+
+    private static SessionFactory sessionFactory;
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection;
+        connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/dbhiber");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "R00T_Tdutybq_>hmtdbx");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+                configuration.setProperties(settings).addAnnotatedClass(User.class);
+                StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                System.out.println("Сессия успешно создана");
+            } catch (HibernateException e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+    public static void closeSession(){
+        sessionFactory.close();
+        System.out.println("Сессия успешно закрыта");
+    }
 }
