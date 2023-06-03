08cd02ecdbe8d5eddd8b139cbf92cd875cd18a83
ВашÐEGo87 <gokov87@vk.com>
2023-06-03 19:12:36 +0300

    version_7.0.0

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,24 +7,11 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-//    private static final String DB_URL = "jdbc:mysql://localhost:3306/dbhiber";
-//    private static final String DB_USERNAME = "root";
-//    private static final String DB_PASSWORD = "R00T_Tdutybq_>hmtdbx";
-//
+
     private static SessionFactory sessionFactory;
-//
-//    public static Connection getConnection() throws SQLException {
-//        Connection connection;
-//        connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-//        return connection;
-//    }
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
